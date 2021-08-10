fd2fdd0fe519fdeb8822a7ceb900e50b591d5a0b
DucHai2003 <duchai22082003@gmail.com>
2021-08-10 15:41:58 +0700

    Lesson1 - Hai Upload

---
+++ Lesson1-Hai/Lesson1-Bai1/Lesson1-Bai1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string soLe = " va la So Le";
+            string soChan = " va la So Chan";
+            string soAm = " va la So Am";
+            string soDuong = " va la So Duong";
+
+            Console.WriteLine("Nhap 1 so n bat ki: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0 && n % 2 == 0)
+            {
+                Console.WriteLine("Day la " + soChan + soDuong);
+            }
+            else if (n > 0 && n % 2 == 1)
+            {
+                Console.WriteLine("Day la " + soLe + soDuong);
+            }
+            else if (n < 0 && n % 2 == 0)
+            {
+                Console.WriteLine("Day la " + soChan + soAm);
+
+            }
+            else if (n < 0 && n % 2 == 1)
+            {
+                Console.WriteLine("Day la " + soLe + soAm);
+            }
+
+        }
+    }
+}
