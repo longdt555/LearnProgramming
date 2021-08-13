eb37c3dbd465dcf76421d54ed479694cc2caa65e
DucHai2003 <88506024+DucHai2003@users.noreply.github.com>
2021-08-13 17:40:37 +0700

    Lesson2 - Hai up
    Lesson2 - Hai up

---
+++ Lesson2/Lesson2.1/Tool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2._1
+{
+    public static class Tool
+    {
+        public static string HSG = "Ban la hoc sinh gioi";
+        public static string HSK = "Ban la hoc sinh kha";
+        public static string HSTB = "Ban la hoc sinh trung binh";
+        public static string HSY = "Ban la hoc sinh kem";
+    }
+}
