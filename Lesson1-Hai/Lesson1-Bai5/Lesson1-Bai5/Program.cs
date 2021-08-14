364f2b9b3703b59a2a7ec01109f7c0808b5a58eb
Yong <longdt@techvify.com.vn>
2021-08-14 23:55:21 +0700

    [ADD] - Comment source

--- Lesson1-Hai/Lesson1-Bai5/Lesson1-Bai5/Program.cs
+++ Lesson1-Hai/Lesson1-Bai5/Lesson1-Bai5/Program.cs
@@ -14,7 +14,7 @@ namespace Lesson1_Bai5
             ForegroundColor = ConsoleColor.Yellow;
 
             EnterOption: int optionSelected = ConvertStringToInt(GetValueFromKeyBoard("tính năng mày muốn dùng")); // 1
-            
+
             // Số lần nhập sai và thời gian nhập sai, nếu nhập sai quá 2 lần thi thoát và in ra số lần nhập sai kèm thời gian nhập sai: Datetime.Now
             switch (optionSelected)
             {
@@ -73,3 +73,103 @@ namespace Lesson1_Bai5
         }
     }
 }
+
+
+/*
+ *
+ *
+using System;
+using System.Text;
+using static System.Console;
+
+namespace Lesson1_Bai5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            OutputEncoding = Encoding.UTF8;
+            var isValid = true;
+            TinhLuong: do
+            {
+                if (!isValid)
+                {
+                    var option = GetValueFromKeyBoard("Bạn có muốn thử lại không (Y/N): ").ToUpper();
+                    switch (option)
+                    {
+                        case "Y":
+                            isValid = true;
+                            break;
+                        case "N":
+                            isValid = false;
+                            break;
+                        default:
+                            isValid = false;
+                            break;
+                    }
+                }
+
+                if (!isValid) break;
+                try
+                {
+                    var bacLuong = Convert.ToInt64(GetValueFromKeyBoard("Nhập bậc lương hiện tại của bạn: "));
+
+                    var ngayCong = Convert.ToInt64(GetValueFromKeyBoard("Nhập ngày công thực tế của bạn: "));
+
+                    var phuCap = Convert.ToInt64(GetValueFromKeyBoard("Nhập phụ cấp của bạn: "));
+
+                    var tienLanh = bacLuong * Constants.Rate;
+
+                    const string commonStr = "Lương của bạn là: ";
+                    WriteLine(ngayCong <= 2 ? $@"{commonStr}{tienLanh * (ngayCong * 2) + phuCap}" : $@"{commonStr}{tienLanh * ngayCong + phuCap}");
+                }
+                catch (Exception)
+                {
+                    WriteLine("Có lỗi xảy ra vùi lòng thử lại sau.");
+                    isValid = false;
+                }
+
+            } while (!isValid);
+
+            var optionExit = GetValueFromKeyBoard("Bạn có muốn thoát chương trình không (Y/N): ").ToUpper();
+            switch (optionExit)
+            {
+                case "Y":
+                    Beep();
+                    WriteLine("Tạm biệt ");
+                    break;
+
+                case "N":
+                    isValid = true;
+                    goto TinhLuong;
+
+                default:
+                    Beep();
+                    WriteLine("Tạm biệt ");
+                    break;
+            }
+        }
+
+        #region [Private functions helper]
+
+        private static string GetValueFromKeyBoard(string message)
+        {
+            Write($@"{message}");
+            return ReadLine();
+        }
+
+
+        #endregion [Private functions helper]
+
+        #region [Common Data]
+
+        public static class Constants
+        {
+            public const double Rate = 650000;
+        }
+
+        #endregion [Common Data]
+
+    }
+}
+ */
