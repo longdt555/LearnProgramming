15e36884bc17c9c27eed91b177982844b69c3545
DucHai2003 <88506024+DucHai2003@users.noreply.github.com>
2021-08-23 19:14:20 +0700

    upload
    upload

---
+++ Assignment 1.1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputEncoding = Encoding.UTF8;
+            ForegroundColor = ConsoleColor.Yellow;
+            //  id, tên, giới tính, tuổi, điểm toán, điểm lý, điểm hóa, điểm trung bình và học lực.
+
+            var students = new List<Student>();
+
+            EnterOption: try {
+                while (true)
+                {
+                    Console.WriteLine("\nCHƯƠNG TRÌNH QUẢN LÍ SINH VIÊN\a");
+                    Console.WriteLine("=========================MENU==========================");
+                    Console.WriteLine("**  1. Thêm sinh viên.                               **");
+                    Console.WriteLine("**  2. Cập nhật thông tin sinh viên bởi ID.          **");
+                    Console.WriteLine("**  3. Xóa sinh viên bởi ID.                         **");
+                    Console.WriteLine("**  4. Tìm kiếm sinh viên theo tên.                  **");
+                    Console.WriteLine("**  5. Sắp xếp sinh viên theo điểm trung bình (GPA). **");
+                    Console.WriteLine("**  6. Sắp xếp sinh viên theo tên.                   **");
+                    Console.WriteLine("**  7. Sắp xếp sinh viên theo ID.                    **");
+                    Console.WriteLine("**  8. Hiển thị danh sách sinh viên.                 **");
+                    Console.WriteLine("**  0. Thoát                                         **");
+                    Console.WriteLine("=======================================================");
+                    Console.Write("Nhập tùy chọn: ");
+                    int key = Convert.ToInt32(Console.ReadLine());
+                     switch (key)
+                    {
+                        case 1:
+                            WriteLine("\n1. Thêm sinh viên.");
+
+                            var student = SinhVien.themSinhVien(); // Tạo ra sinh viên
+                            student.IdSinhVien = SinhVien.GenerateId(students); // Cập nhật id cho sinh viên đó là unique
+
+                            students.Add(student); // Thêm sinh viên đó vào danh sách sinh viên
+                            WriteLine("\nThêm sinh viên thành công!");
+
+                            break;
+
+                        case 2:
+                            WriteLine("2. Cập nhật thông tin sinh viên bởi ID.");
+                            SinhVien.UpdateSinhVien(students);
+                            break;
+
+                        case 3:
+                            WriteLine("3. Xóa sinh viên bởi ID.");
+                            SinhVien.DeleteSinhVien(students);
+                            break;
+
+                        case 4:
+                            WriteLine("4. Tìm kiếm sinh viên theo tên. ");
+                            SinhVien.SearchSinhVien(students);
+                            break;
+
+                        case 5:
+                            WriteLine("5. Sắp xếp sinh viên theo điểm trung bình (GPA).");
+                            SinhVien.SortByDiemTB(students);
+                            SinhVien.Printf(students);
+                            break;
+
+                        case 6:
+                            WriteLine("6. Sắp xếp sinh viên theo tên.");
+                            SinhVien.SortByName(students);
+                            SinhVien.Printf(students);
+                            break;
+
+                        case 7:
+                            WriteLine("7. Sắp xếp sinh viên theo ID.");
+                            SinhVien.SortByID(students);
+                            SinhVien.Printf(students);
+                            break;
+
+                        case 8:
+                            WriteLine("\n8. Hiển thị danh sách sinh viên.");
+                            if (students.Count() > 0)
+                            {
+                                SinhVien.Printf(students);
+                            }
+                            else
+                            {
+                                WriteLine("Chưa có phần tử nào!");
+                            }
+                            break;
+                        case 0:
+                            Console.WriteLine("\nBạn đã chọn thoát chương trình!");
+                            return;
+                        default:
+                            Console.WriteLine("\nKhông có chức năng này!");
+                            Console.WriteLine("\nHãy chọn chức năng trong hộp menu.");
+                            break;
+                    }
+                }
+            }catch(Exception a)
+            {
+                WriteLine("Hãy nhập số !!!");
+                goto EnterOption;
+            }
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ Assignment 1.1/SinhVien.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Assignment
+{
+    public static class SinhVien
+    {
+        public static Student themSinhVien()
+        {
+            Student student = new Student();
+            student.Name = Helper.GetValue("Nhập tên sinh viên: ");
+            student.GioiTinh = Helper.GetValue("Nhập giới tính sinh viên: ");
+            student.AgeSinhVien = Convert.ToInt32(Helper.GetValue("Nhập tuổi sinh viên: "));
+            student.DiemToan = Convert.ToSingle(Helper.GetValue("Nhập điểm toán của sinh viên: "));
+            student.DiemLy = Convert.ToSingle(Helper.GetValue("Nhập điểm lý của sinh viên: "));
+            student.DiemHoa = Convert.ToSingle(Helper.GetValue("Nhập điểm hóa của sinh viên: "));
+            student.DiemTB = ((student.DiemToan + student.DiemLy + student.DiemHoa) / 3);
+            student.IdSinhVien = 0;
+           
+            WriteLine("Điểm TB của sinh viên là: " + student.DiemTB);
+            if (student.DiemTB >= 8)
+                student.HocLuc = "Học sinh giỏi";
+
+            if (student.DiemTB < 8 && student.DiemTB >= 6.5)
+                student.HocLuc = "Học sinh khá";
+
+            if (student.DiemTB < 6.5 && student.DiemTB >= 5)
+                student.HocLuc = "Học sinh TB";
+
+            if (student.DiemTB < 5)
+            {
+                student.HocLuc = "Học sinh yếu";
+            }
+            return student;
+
+
+        }
+
+        public static int GenerateID(List<Student> students)
+        {
+            return students.Max(r => r.IdSinhVien) + 1;
+        }
+
+        public static int GenerateId(List<Student> students)
+        {
+            int max = 1;
+            if(students!= null && students.Count > 0)
+            {
+                max = students[0].IdSinhVien;
+                foreach (var IdSinhVien in students)
+                {
+                    if(max < IdSinhVien.IdSinhVien)
+                    {
+                        max = IdSinhVien.IdSinhVien;
+                    }
+                }
+                max++;
+            }
+            return max;
+        }
+        public static int SoLuongSinhVien(List<Student> students)
+        {
+            int Count = 0;
+            if ( students!= null)
+            {
+                Count = students.Count;
+            }
+            return Count;
+        }
+
+        public static void UpdateSinhVien(List<Student> students)
+        {
+                Printf(students);
+                Student student = new Student();
+                int id = Convert.ToInt32(Helper.GetValue("Nhập id sinh viên: "));
+                students.Find(x => x.IdSinhVien == id);
+                students.Remove(student);
+                if(students == null)
+                {
+                    WriteLine("Không tìm thấy học sinh này!");
+                }
+                else
+                { 
+                    Write("Nhập họ tên: ");
+                    student.Name = Console.ReadLine();
+                    student.GioiTinh = Helper.GetValue("Nhập giới tính: ");
+                    student.AgeSinhVien = Convert.ToInt32(Helper.GetValue("Nhập tuổi: "));
+                    student.DiemToan = Convert.ToSingle(Helper.GetValue("Nhập điểm toán: "));
+                    student.DiemLy = Convert.ToSingle(Helper.GetValue("Nhập điểm lý: "));
+                    student.DiemHoa = Convert.ToSingle(Helper.GetValue("Nhập điểm hóa: "));
+                }
+            
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Sửa thành công!");
+            Printf(students);
+
+        }
+       
+        public static void SearchSinhVien(List<Student> students)
+        {
+            List<Student> student = new List<Student>();
+            string name = Helper.GetValue("Nhập tên sinh viên: ");
+            student = students.FindAll(x => x.Name.Contains(name));
+            if (student != null)
+            {
+                Printf(student);
+                WriteLine("Tìm thấy rồi!");
+            }
+            else
+            {
+                WriteLine("Không tìm thấy học sinh này!");
+            }
+        }
+
+        public static void DeleteSinhVien(List<Student> students)
+        {
+            int id = int.Parse(Helper.GetValue("Nhập id sinh viên: "));
+            int index = students.FindIndex(x => x.IdSinhVien == id);
+            if (index > 0)
+            {
+                students.RemoveAt(index);
+                WriteLine("Đã xóa sinh viên");
+            }
+            else
+            {
+                WriteLine("Không tìm thấy học sinh!!!");
+            }     
+        }
+
+        public static void SortByDiemTB(List<Student> students)
+        {
+            students.Sort(delegate (Student student, Student student1)
+            {
+                return student.DiemTB.CompareTo(student1.DiemTB);
+               
+               
+            });
+            
+        }
+
+        public static void SortByName(List<Student> students)
+        {
+            students.Sort(delegate (Student student, Student student1) {
+                return student.Name.CompareTo(student1.Name);
+            });
+        }
+
+        
+
+        public static void Printf(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                WriteLine($"{student.IdSinhVien}\t{student.Name}\t {student.GioiTinh}\t {student.AgeSinhVien}\t" +
+                    $"{student.DiemToan}\t{student.DiemLy}\t{student.DiemHoa}\t{student.DiemTB}\t{student.HocLuc}");
+
+            }
+        }
+
+        public static void SortByID(List<Student> students)
+        {
+            students.Sort(delegate (Student student, Student student1) {
+                return student1.IdSinhVien.CompareTo(student1.IdSinhVien);
+            });
+        }
+
+
+    }
+    public struct Student
+    {
+        public string Name;
+        public string GioiTinh;
+        public int IdSinhVien;
+        public int AgeSinhVien;
+        public float DiemToan;
+        public float DiemLy;
+        public float DiemHoa;
+        public float DiemTB;
+        public string HocLuc;
+
+       
+    }
+}
