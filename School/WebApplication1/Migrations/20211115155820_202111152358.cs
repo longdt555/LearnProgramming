880ffe65eb680e1654f51e76dafafdb19ee85f4f
DucHai2003 <duchai22082003@gmail.com>
2021-11-16 17:15:41 +0700

    hai update 16/11

---
+++ School/WebApplication1/Migrations/20211115155820_202111152358.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreManagement.Migrations
+{
+    public partial class _202111152358 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- School/WebApplication1/wwwroot/js/app/role.js
+++ School/WebApplication1/wwwroot/js/app/role.js
@@ -1,1 +1,62 @@
-﻿
+﻿const { viewport } = require("@popperjs/core");
+
+
+<script type="text/javascript">
+    //$('#btnsubmit').click(function(){
+        //    alert('haha');
+        //});
+
+
+        function submitForm() {
+            debugger; // sử dụng để debug js
+            var isValid = true;
+            $('#errMsg').html('');
+
+            // lấy giá trị của các trường dữ liệu (js / jquery)
+            var name = $('#name').val();
+            var name1 = $('#name1').val();
+
+
+            <div id="loader" data-request-url="@Url.Action(" /Views/Save", "/Views/Role")" ></div>
+
+
+
+            // xử lý dữ liệu nếu có - cộng trừ nhân chia, ghép dữ liệu, xóa dữ liệu, validate(ABC_9)
+            // name == name1
+
+            if (name !== name1) {
+                $('#errMsg').html("<span style='color:red'>[Name] phải bằng [Name1]</span>");
+                isValid = false;
+            }
+
+            // gọi hành động BE
+
+            if (isValid) {
+                var model = {
+                    name: $('#name').val()
+                };
+
+                
+
+                $.ajax({
+                    url: "#loader", // Url of backend (can be python, php, etc..)*/
+                    type: "POST", // data type (can be get, post, put, delete)
+                    data: model, // data in json format
+                    //async : false, // enable or disable async (optional, but suggested as false if you need to populate data afterwards)
+                    success: function (response) {
+                        if (response.success) {
+                            $('#errMsg').html("<span style='color:red'>Xin chào " + response.data + "</span>");
+                        }
+                    },
+                    error: function (response) {
+                        console.log("hihi");
+                    }
+                });
+            };
+
+            // xử lý response trả về (data, status, error)
+
+            // thực hiện thay đổi UI
+        };
+
+</script>
