8b0d4f152cb2eb81cf95064cc0b411d894cd4deb
YONG <longdt@sphinxjsc.com>
2021-11-23 22:31:37 +0700

    [UPDATE]

--- School/WebApplication1/wwwroot/js/app/account.js
+++ School/WebApplication1/wwwroot/js/app/account.js
@@ -51,3 +51,26 @@ function deleteRecord(id) {
         }
     });
 };
+
+
+function ShowAddModal(event) {
+    event.preventDefault();
+    $('#common-modal').modal('show');
+}
+
+function ReloadList() {
+    $.ajax({
+        url: "/Account/Search", // Url of backend (can be python, php, etc..)*/
+        type: "GET", // data type (can be get, post, put, delete)
+        data: {
+            pageIndex: 1,
+            pageSize: 20
+        }, // data in json format
+        success: function (response) { // request returns successed
+            $('#account-list').html(response);
+        },
+        error: function (response) { // // request returns failed
+            console.log("error");
+        }
+    });
+}
