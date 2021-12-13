877bc91500b4ff09c6793a6497d3d20b520ba740
YONG <longdt@sphinxjsc.com>
2021-12-13 21:43:33 +0700

    [UPDATE]

---
+++ School/WebApplication1/wwwroot/js/common.js
@@ -0,0 +1,21 @@
+﻿checkExistClass = (parentClass, childClass) => {
+    return $(`${parentClass}`).hasClass(`${childClass}`);
+};
+
+removeClass = (parentClass, childClass) => {
+    if ($(`${parentClass}`).hasClass(`${childClass}`))
+        $(`${parentClass}`).removeClass(`${childClass}`);
+};
+
+addClass = (parentClass, childClass) => {
+    if (!$(`${parentClass}`).hasClass(childClass))
+        $(`${parentClass}`).addClass(childClass);
+};
+
+getAttrValue = (parentClass, attrName) => {
+    return $(parentClass).attr(attrName);
+};
+
+getSelectValue = id => {
+    return $(id).value;
+};
